93dff5914065b61b1a69fcadbc244e79938212b8
Pranathi-pamu <chinnupamu11@gmail.com>
2024-10-14 14:41:53 +0530

    Initialise file

---
+++ c to f.c
@@ -0,0 +1,39 @@
+#include <math.h>
+
+int main() 
+{
+     float c,f;
+     printf("enter the c value ");
+     scanf("%f",&c);
+     f=(1.8*c)+32;
+     
+     
+     
+      printf("the temp in f %f",f);
+     
+     
+    
+  
+ 
+       
+         
+       
+         
+       
+       
+     
+     
+      
+      
+      
+      
+      
+     
+     
+      
+ }    
+ 
+     
+
+
+    
